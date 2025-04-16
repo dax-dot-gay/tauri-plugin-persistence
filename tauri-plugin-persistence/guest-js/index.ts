aa6f66910899044737d92295e3d7c185db106295
Dax Harris <mvh3875@rit.edu>
2025-04-16 17:04:55 -0400

    extra types

--- tauri-plugin-persistence/guest-js/index.ts
+++ tauri-plugin-persistence/guest-js/index.ts
@@ -6,7 +6,7 @@ import {
     Transaction,
     FileHandle,
 } from "./context";
-import { JsonValue, FileHandleMode } from "./commands";
+import { JsonValue, FileHandleMode, UpdateResult, Error } from "./commands";
 
 export { Result, Context, Database, Collection, Transaction, FileHandle };
-export type { Res, JsonValue, FileHandleMode };
+export type { Res, JsonValue, FileHandleMode, UpdateResult, Error };
