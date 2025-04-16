6f4a450ab1b6d576ef82ea76f0980f4e3ffdc0f2
Dax Harris <mvh3875@rit.edu>
2025-04-16 17:18:43 -0400

    setup permissions

--- tauri-plugin-persistence/build.rs
+++ tauri-plugin-persistence/build.rs
@@ -1,8 +1,33 @@
-const COMMANDS: &[&str] = &["ping"];
+const COMMANDS: &[&str] = &[
+    "context",
+    "database",
+    "file_handle",
+    "database_get_collections",
+    "database_close",
+    "database_start_transaction",
+    "database_commit_transaction",
+    "database_rollback_transaction",
+    "collection_count_documents",
+    "collection_update_documents",
+    "collection_delete_documents",
+    "collection_create_index",
+    "collection_drop_index",
+    "collection_drop",
+    "collection_insert_documents",
+    "collection_find_many_documents",
+    "collection_find_one_document",
+    "file_close",
+    "file_write_text",
+    "file_write_bytes",
+    "file_read_text",
+    "file_read_bytes",
+    "get_context_base_path",
+    "get_absolute_path_to",
+    "create_directory",
+    "remove_directory",
+    "remove_file",
+];
 
 fn main() {
-  tauri_plugin::Builder::new(COMMANDS)
-    .android_path("android")
-    .ios_path("ios")
-    .build();
+    tauri_plugin::Builder::new(COMMANDS).build();
 }
